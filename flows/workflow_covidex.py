c96dd92a9bbb31b77163900f99838fb4eda35620
shreyas rao <shreyassureshrao@gmail.com>
2024-09-01 11:17:55 +0530

    modified

--- flows/workflow_covidex.py
+++ flows/workflow_covidex.py
@@ -1,3 +1,11 @@
+# Step 1: pip install prefect
+# In the end, there are two ways to execute
+# 1. python workflow_covidex.py which starts the prefect server and runs on prefect cloud dashboard
+# 2a. > prefect deployment build flows/workflow_covidex.py:main_flow -n "Covid DS Workflow Deployment" --output deployment.yaml
+# 2b. prefect deployment apply deployment.yaml   
+# First step creates a deployment.yaml file in "flows directory" and second step applies the deployment to prefect cloud.
+# It provides the UI to view the deployment in the cloud
+
 from prefect import flow, task, get_run_logger
 import subprocess
 import os
